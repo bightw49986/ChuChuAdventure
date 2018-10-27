0cd9a5842f6af389f73c23215381b0658b737424
易儒 <>
2018-10-28 04:28:07 +0800

    戰鬥框架完成一半: 攻擊盒架構完成、防禦盒還沒

---
+++ Assets/Scripts/AI and Battle/AISystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AISystem
+{
+    public interface IAI_SimpleMove
+    {
+        float fCurrentSpeed { get; set; } 
+        float fMaxSpeed { get; set; }
+        Vector3 vVelocity { get; set; }
+        Vector3 vMaxAcceleration { get; set; }
+        Vector3 vMaxVelocity { get; set; }
+        Vector3 vPositionNextFrame { get; set; }
+        bool bAnimated { get; set; }
+        bool bUseRootMotion { get; set; }
+        void GetRootMotionSpeed();
+    }
+
+    public interface IAI_CollisionAvoid
+    {
+        Transform tNearestObstacle { set; get; }
+        LayerMask CollisionLayer { set; get; }
+        float fProbeLength { set; get; }
+        float fDetectionRadius { set; get; }
+        float fDotLastFrame { set; get; }
+
+        bool DetectCollision();
+
+        void AvoidCollision();
+    }
+}
---
+++ Assets/Scripts/AI and Battle/DefendBox.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleSystem
+{
+    /// <summary>
+    /// 定義要使用受擊盒的角色所需的資料
+    /// </summary>
+    public interface IDefender
+    {
+        List<DefendBox> DefendBoxes { get; set; }
+        HashSet<EAttackerType> TakeDamageFrom { get; set; }
+        float MaxHP { get; set; }
+        float HP { get; set; }
+        float MaxToughness { get; set; }
+        float Toughness { get; set; }
+        float RecoverTime { get; set; }
+        bool IsHarmless { get; set; }
+
+        void InitDefendBoxs();
+    }
+
+
+    /// <summary>
+    /// 防禦(受擊)盒的基本類別
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class DefendBox : MonoBehaviour
+    {
+        /// <summary>
+        /// 這個受擊盒的主人
+        /// </summary>
+        /// <value>受擊盒的擁有者</value>
+        public IDefender Host { get; private set; }
+
+        /// <summary>
+        /// 這個受擊盒要接收哪些種類的傷害
+        /// </summary>
+        /// <value>The type of the take damage.</value>
+        public HashSet<EAttackerType> TakeDamageType { get; set; }
+
+        /// <summary>
+        /// 這個受擊盒的Collider
+        /// </summary>
+        /// <value>Collider</value>
+        public Collider Collider { get; private set; }
+
+
+
+        /// <summary>
+        /// 啟動時，初始化受擊資訊
+        /// </summary>
+        void OnEnable()
+        {
+            TakeDamageType = Host.TakeDamageFrom;
+        }
+
+
+        /// <summary>
+        /// 根據攻擊盒那邊送來的傷害值計算傷害資料，判定傷害事件發生
+        /// </summary>
+        /// <param name="fDamage">傷害值</param>
+        public void CalculateDamage(float fDamage)
+        {
+
+        }
+
+        /// <summary>
+        /// 受到傷害但沒發生硬直的事件
+        /// </summary>
+        public event Action DamageTaken;
+        public virtual void OnDamageTaken()
+        {
+            if (DamageTaken != null) DamageTaken();
+        }
+
+        /// <summary>
+        /// 受到傷害且強韌度不足，導致硬直發生的事件
+        /// </summary>
+        public event Action DamageTakenFreezed;
+        public virtual void OnDamageTakenFreezed()
+        {
+            if (DamageTakenFreezed != null) DamageTakenFreezed();
+        }
+
+        /// <summary>
+        /// 受到傷害且HP不足，導致宿主死亡的事件
+        /// </summary>
+        public event Action DamageTakenDied;
+        public virtual void OnDamageTakenDied()
+        {
+            if (DamageTakenDied != null) DamageTakenDied();
+        }
+    }
+}
